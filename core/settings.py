dcb0daec97d781bffb88186b15ad092eb7f469d1
sobirjonov_me <huntsman6004@gmail.com>
2022-11-05 18:02:06 +0500

    media files setting

--- core/settings.py
+++ core/settings.py
@@ -131,3 +131,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media Files
+MEDIA_ROOT = BASE_DIR / 'media-files'
+
+MEDIA_URL = '/media/'
