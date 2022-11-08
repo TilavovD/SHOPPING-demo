d6ecefa3c859cc8fcb8c4d3af8dcebacb25f5446
sobirjonov_me <huntsman6004@gmail.com>
2022-11-08 20:40:43 +0500

    stripe settins added

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from .views import CartListAPIView, CartItemAPIView, AddressListAPIView
+from .views import (
+    CartListAPIView,
+    CartItemAPIView,
+    AddressListAPIView,
+    StripeConfigView,
+    StripeSessionView,
+
+)
 
 app_name = 'orders'
 
@@ -8,5 +15,8 @@ urlpatterns = [
     path('cart/', CartListAPIView.as_view(), name='cart-list'),
     path('cart/<int:pk>/', CartItemAPIView.as_view(), name='cart-item'),
     path('addressess/', AddressListAPIView.as_view(), name='address-list'),
+
+    path('payment/', StripeSessionView.as_view(), name='stripe-session'),
+    path('payment/stripe-config', StripeConfigView.as_view(), name='stripe-config'),
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,8 @@
+import stripe
+from django.conf import settings
+
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import CartItem, Address
@@ -41,3 +45,48 @@ class CartItemAPIView(RetrieveUpdateDestroyAPIView):
 class AddressListAPIView(ListCreateAPIView):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+
+
+class StripeConfigView(APIView):
+    """
+    StripeConfigView is the API of configs resource, and
+    responsible to handle the requests of /config/ endpoint.
+    """
+    def get(self, request):
+        config = {
+            "publishable_key": settings.STRIPE_PUBLISHABLE_KEY
+        }
+        return Response(config)
+
+
+class StripeSessionView(APIView):
+    def get(self, request):
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+
+        pay_data = {
+            'price_data': {
+                "currency": "usd",
+                "unit_amount": 1000,
+                "product_data": {
+                    "name": "product_name",
+                    "images": [],
+                }
+            },
+            "quantity": 1,
+        }
+
+        checkout_session = stripe.checkout.Session.create(
+            success_url=settings.PAYMENT_SUCCESS_URL,
+            cancel_url=settings.PAYMENT_CANCEL_URL,
+            mode='payment',
+            line_items=[
+                pay_data,
+            ]
+        )
+
+        return Response(
+            {
+                'sessionId': checkout_session['id'],
+                'session_url': checkout_session['url']
+             }
+        )
