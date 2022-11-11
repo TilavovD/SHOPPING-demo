c9266af21b30da2e66758d78a1565511d4b9ba7f
TilavovD <tilavov.dostonbek@gmail.com>
2022-11-11 15:59:21 +0500

    token authentication config

--- users/models.py
+++ users/models.py
@@ -82,3 +82,15 @@ class User(AbstractUser):
 
     def __str__(self):
         return str(self.phone_number)
+
+
+
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
