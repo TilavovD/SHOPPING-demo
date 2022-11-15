153e1b3250979d35c9a646236e9c94113070b31d
sobirjonov_me <huntsman6004@gmail.com>
2022-11-15 16:15:08 +0500

    users/urls config

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,10 @@ from rest_framework.authtoken import views
 from .views import UserDetailView, UserCreateView, CheckSecretCodeAPIView, LoginView
 
 urlpatterns = [
-    path("detail", UserDetailView.as_view(), name='user_detail'),
-    path("create", UserCreateView.as_view(), name='user_create'),
-    path('api-token-auth', views.obtain_auth_token),
-    path('verify_secret_code', CheckSecretCodeAPIView.as_view(), name="verify_code"),
-    path('login', LoginView.as_view(), name="login")
+    path("detail/", UserDetailView.as_view(), name='user_detail'),
+    path("create/", UserCreateView.as_view(), name='user_create'),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('verify_secret_code/', CheckSecretCodeAPIView.as_view(), name="verify_code"),
+    path('login/', LoginView.as_view(), name="login")
 
 ]
