3b721ea6a8c5620e0c75455dfab03b0c1a82f98f
TilavovD <tilavov.dostonbek@gmail.com>
2022-11-17 16:53:18 +0500

    connected swagger to project

--- core/urls.py
+++ core/urls.py
@@ -14,14 +14,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf import settings
 from django.conf.urls.static import static
 from users.views import LoginView
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+...
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Shopping API",
+      default_version='v1',
+      description="This project is demo for shopping api",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="admin@s-orca.uz"),
+      license=openapi.License(name="S-Orca License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
     path("api/v1/user/", include("users.urls")),
